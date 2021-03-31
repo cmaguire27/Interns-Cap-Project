8f2187f921f5fb8c11527c893b5a907c5e065626
ronanmmurphy <ronan-murphy@outlook.ie>
2021-03-31 17:45:45 +0100

    cubeE

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
 import React, { Suspense, useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-
+import GenericCubeEBody from './Components/CubeEBody'
+import FemaleConnector from './Components/Female'
+import MaleConnector from './Components/MaleConnector'
+import {OrbitControls} from '@react-three/drei'
 import './App.css';
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+function Cube1(props){
+  
+  return(
+    //frontright
+    //back left
+    //bottom
+    //front left
+    //back right
+    <group
+    name="cubex">
+    <GenericCubeEBody/>
+    
+    <FemaleConnector  position={[0,0,0.165]} rotation={[90*(Math.PI/180),0,0]}/> 
+    <FemaleConnector  position={[0,0,-0.165]} rotation={[270*(Math.PI/180),0,0]}/>
+    <FemaleConnector  position={[0,-0.165,0]} rotation={[180*(Math.PI/180),0,0]}/>
+    <FemaleConnector   position={[-0.165,0,0]} rotation={[90*(Math.PI/180),0,90*(Math.PI/180)]}/>
+    <FemaleConnector   position={[0.165,0,0]} rotation={[90*(Math.PI/180),0,270*(Math.PI/180)]}/>
+    <MaleConnector  position={[0,0.16,0]} rotation={[0*(Math.PI/180),0,0]} />
+    
+      </group>
   )
-}
 
+}
 function App() {
   return (
-    <Canvas>
+    <Canvas 
+    colorManagement
+    shadowMap
+    camera={{position: [-1,1,1], fov: 40}}
+    invalidateFrameloop 
+    style={{ background: "#fff" }}>
+    <OrbitControls
+                enableDamping={true}
+                dampingFactor={0.25}
+                rotateSpeed={0.4}
+                keyPanSpeed={0.4}
+                screenSpacePanning={true}
+                zoomSpeed={0.6}
+                enablePan={true}
+                panSpeed={0.4}
+                minDistance={-500}
+                maxDistance={1000}
+              />
+
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      
-      <Box position={[0, 0, 0]} />
+      <Suspense fallback={null}>
+      <Cube1 />
+      </Suspense>
     </Canvas>
   );
 }
