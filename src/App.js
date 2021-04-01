91137b41593ff9332f3da1630672d102b07d048e
ronanmmurphy <ronan-murphy@outlook.ie>
2021-04-01 11:05:11 +0100

    box and female

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Suspense, useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import {OrbitControls } from '@react-three/drei'
+import {OrbitControls,  useGLTF  } from '@react-three/drei'
 import GenericCubeEBody from './Components/CubeEBody'
 import FemaleConnector from './Components/Female'
 import MaleConnector from './Components/MaleConnector'
 import './App.css';
+
+function Model(props) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('Female.glb')
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="#000"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+    document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(hovered ? cursor : auto)}'), auto`
+  }, [hovered])
+  
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh material={materials['default']} 
+      geometry={nodes.Triangle_v32.geometry} 
+      material-color={props.color}
+      scale={[0.01, 0.01, 0.01]}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}>
+      <meshStandardMaterial color={hovered||active ? 'hotpink' : 'orange'} />  
+      </mesh>
+    </group>
+  )
+}
+
 
 function Box(props) {
   // This reference will give us direct access to the mesh
@@ -60,7 +87,8 @@ function App() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Suspense fallback={null}>
-      <Box position={[0, 0, 0]} />
+      <Model  position={[0,0,0.165]} rotation={[90*(Math.PI/180),0,0]}/> 
+      <Box position={[1,0,-0.3]}/>
       </Suspense>
       
   
